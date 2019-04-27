d860221f4a79f07f7b08715c1ae650fc5de2af26
JoseLuis <observadorxpl@gmail.com>
2019-04-27 18:48:12 -0500

    Agregado Clase ConatinnerActivity y BottomBar, falta Fragments

---
+++ app/src/main/java/com/joseluis/login/ContainnerActivity.java
@@ -0,0 +1,13 @@
+package com.joseluis.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContainnerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_containner);
+    }
+}
